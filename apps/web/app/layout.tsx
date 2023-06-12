cf8c8aba1fe79cb40e2320cd0b0668be6bbaae90
Leonardo Almeida Marques <leo@sydl1500661.oc.rabonet.com>
2023-06-12 14:47:47 +0200

    added analytics to it

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,6 @@
 import "@iamleo/tailwind/global.css";
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: {
@@ -6,6 +8,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7KGSFQYN7V"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-7KGSFQYN7V');
+        `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
