769f8f3a9840eab81bfb49381f356e99c0787aa5
Leonardo Almeida Marques <leo@sydl1500661.oc.rabonet.com>
2023-06-21 16:37:42 +0200

    testing ping on vercel

---
+++ apps/blog_api/app/ping/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  return new Response("<GET> Hello, Next.js!", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
+export async function OPTIONS(request: Request) {
+  return new Response("<OPTIONS> Hello, Next.js!", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
+export async function POST(request: Request) {
+  return new Response("<POST> Hello, Next.js!", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
